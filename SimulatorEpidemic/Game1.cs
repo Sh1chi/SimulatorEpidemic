49885abce9e9ee5c4e7a9b115c9c87a0a3ed5b1d
Shevtsov Artem <shevcov_2k4_2@mail.ru>
2024-06-05 16:57:55 +0700

    Добавление текстуры и улучшение логики движения
    - Перешел на использование текстуры кружка вместо программной отрисовки
    - Добавил объекту человека скорость и рандомное направление движения
    - Реализовал корректную обработку столкновений со стенами, учитывая радиус кружка
    - Вынес логику движения и изменения направления в класс Human для лучшей структуры кода

--- SimulatorEpidemic/Game1.cs
+++ SimulatorEpidemic/Game1.cs
@@ -1,84 +1,70 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace SimulatorEpidemic
 {
+    // Основной класс игры, наследующийся от Game
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Human _human;
-        private Texture2D _circleTexture;
+        private GraphicsDeviceManager _graphics; // Управляет графическими устройствами
+        private SpriteBatch _spriteBatch; // Отвечает за пакетную отрисовку спрайтов
+        private Human _human; // Объект человека в игре
+        private Texture2D _humanTexture; // Текстура для отображения человека
+        private Random random; // Объект для генерации случайных чисел
 
+        // Конструктор игры
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            Content.RootDirectory = "Content"; // Директория для контента
+            IsMouseVisible = true; // Делает курсор мыши видимым
+            random = new Random();
         }
 
+        // Инициализация игры
         protected override void Initialize()
         {
             base.Initialize();
         }
 
+        // Загрузка контента игры
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Создание текстуры круга 20x20
-            _circleTexture = CreateCircleTexture(10);
+            // Загрузка текстуры человека
+            _humanTexture = Content.Load<Texture2D>("Human");
 
-            // Инициализация человека
-            _human = new Human(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            // Инициализация объекта человека с размерами экрана и радиусом, основанным на ширине текстуры
+            _human = new Human(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, _humanTexture.Width / 2);
         }
 
-        private Texture2D CreateCircleTexture(int radius)
-        {
-            int diameter = radius * 2;
-            Texture2D texture = new Texture2D(GraphicsDevice, diameter, diameter);
-            Color[] colorData = new Color[diameter * diameter];
-
-            float radiussq = radius * radius;
-
-            for (int x = 0; x < diameter; x++)
-            {
-                for (int y = 0; y < diameter; y++)
-                {
-                    int index = x * diameter + y;
-                    Vector2 pos = new Vector2(x - radius, y - radius);
-                    if (pos.LengthSquared() <= radiussq)
-                    {
-                        colorData[index] = Color.White;
-                    }
-                    else
-                    {
-                        colorData[index] = Color.Transparent;
-                    }
-                }
-            }
-
-            texture.SetData(colorData);
-            return texture;
-        }
-
+        // Обновление логики игры
         protected override void Update(GameTime gameTime)
         {
+            // Проверка нажатия кнопки "Back" на геймпаде или клавиши "Escape" на клавиатуре для выхода из игры
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Логика обновления
+            // Обновление состояния человека
+            _human.Update(gameTime);
 
             base.Update(gameTime);
         }
 
+        // Отрисовка игры
         protected override void Draw(GameTime gameTime)
         {
+            // Очистка экрана цветом "CornflowerBlue"
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            // Начало пакетной отрисовки
             _spriteBatch.Begin();
-            _human.Draw(_spriteBatch, _circleTexture);
+            // Отрисовка человека с использованием текстуры
+            _human.Draw(_spriteBatch, _humanTexture);
+            // Завершение пакетной отрисовки
             _spriteBatch.End();
 
             base.Draw(gameTime);
--- SimulatorEpidemic/Human.cs
+++ SimulatorEpidemic/Human.cs
@@ -6,42 +6,120 @@ namespace SimulatorEpidemic
 {
     public class Human
     {
+        // Перечисление состояний здоровья человека
         public enum HealthState
         {
-            Healthy,
-            Infected,
-            Recovered
+            Healthy,   // Здоров
+            Infected,  // Инфицирован
+            Recovered  // Выздоровевший
         }
 
-        private Vector2 _position;
-        public HealthState State { get; private set; }
+        private Vector2 _position;       // Позиция человека на экране
+        private Vector2 _direction;      // Направление движения человека
+        private float _speed;            // Скорость движения человека
+        private int _screenWidth;        // Ширина экрана
+        private int _screenHeight;       // Высота экрана
+        private float _radius;           // Радиус круга, представляющего человека
+        private Random random;           // Объект для генерации случайных чисел
 
-        public Human(int screenWidth, int screenHeight)
+        public HealthState State { get; private set; }  // Текущее состояние здоровья человека
+
+        private float changeDirectionTimer;   // Таймер для изменения направления движения
+        private float timeUntilNextChange;    // Время до следующего изменения направления
+
+        // Конструктор человека, инициализирующий его позицию, состояние здоровья и параметры движения
+        public Human(int screenWidth, int screenHeight, float radius)
         {
-            Random random = new Random();
-            _position = new Vector2(random.Next(0, screenWidth - 20), random.Next(0, screenHeight - 20));
+            random = new Random();
+            _screenWidth = screenWidth;
+            _screenHeight = screenHeight;
+            _radius = radius;
+            _position = new Vector2(random.Next((int)_radius, screenWidth - (int)_radius), random.Next((int)_radius, screenHeight - (int)_radius));
+            _speed = 100f;
+            _direction = GetRandomDirection();
+
+            // Инициализация таймера для изменения направления движения
+            ResetChangeDirectionTimer();
+
             State = HealthState.Healthy; // Начальное состояние - здоров
         }
 
+        // Метод для обновления состояния человека
+        public void Update(GameTime gameTime)
+        {
+            // Обновление позиции человека
+            _position += _direction * _speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Проверка столкновения с границами экрана и изменение направления при необходимости
+            if (_position.X - _radius <= 0 || _position.X + _radius >= _screenWidth)
+            {
+                _direction.X = -_direction.X;
+                _position.X = MathHelper.Clamp(_position.X, _radius, _screenWidth - _radius);
+            }
+
+            if (_position.Y - _radius <= 0 || _position.Y + _radius >= _screenHeight)
+            {
+                _direction.Y = -_direction.Y;
+                _position.Y = MathHelper.Clamp(_position.Y, _radius, _screenHeight - _radius);
+            }
+
+            // Обновление таймера для изменения направления движения
+            changeDirectionTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (changeDirectionTimer >= timeUntilNextChange)
+            {
+                _direction = GetRandomDirection();
+                ResetChangeDirectionTimer();
+            }
+        }
+
+        // Метод для отрисовки человека
         public void Draw(SpriteBatch spriteBatch, Texture2D texture)
         {
             Color color;
+            // Выбор цвета в зависимости от состояния здоровья
             switch (State)
             {
                 case HealthState.Healthy:
-                    color = Color.Green;
+                    color = Color.Green; // Здоровый - зеленый
                     break;
                 case HealthState.Infected:
-                    color = Color.Red;
+                    color = Color.Red; // Инфицированный - красный
                     break;
                 case HealthState.Recovered:
-                    color = Color.Blue;
+                    color = Color.Blue; // Выздоровевший - синий
                     break;
                 default:
-                    color = Color.White;
+                    color = Color.White; // Неизвестное состояние - белый
                     break;
             }
-            spriteBatch.Draw(texture, _position, color);
+
+            // Отрисовка текстуры с соответствующим цветом на текущей позиции
+            spriteBatch.Draw(
+                texture,
+                _position,
+                null,
+                color,
+                0f,
+                new Vector2(texture.Width / 2, texture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+        }
+
+        // Метод для получения случайного направления движения
+        private Vector2 GetRandomDirection()
+        {
+            // Создание случайного вектора направления
+            float angle = (float)(random.NextDouble() * Math.PI * 2);
+            return new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+        }
+
+        // Метод для сброса таймера и установки нового случайного интервала для изменения направления
+        private void ResetChangeDirectionTimer()
+        {
+            changeDirectionTimer = 0f;
+            timeUntilNextChange = (float)(random.NextDouble() * 3) + 1; // Случайный интервал от 1 до 4 секунд
         }
     }
 }
